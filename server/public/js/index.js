c87379d3a44264e5296671758dded34a640051df
Mark Sturdevant <mark.sturdevant@ibm.com>
2018-02-09 09:28:08 -0800

    Add web ui (#28)
    * Add web ui
    
    * Fix title, fixes tests

---
+++ server/public/js/index.js
@@ -0,0 +1,131 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/* eslint-env browser */
+
+/**
+ * Add detected object info as a row in the table.
+ * @param {Object} table
+ * @param {string} cellType
+ * @param {[]} values
+ */
+function addRow(table, cellType, values) {
+  const row = document.createElement('tr');
+  for (let i = 0; i < values.length; i++) {
+    const val = values[i];
+    const cell = document.createElement(cellType);
+    const text = document.createTextNode(val);
+    cell.appendChild(text);
+    row.appendChild(cell);
+  }
+  table.appendChild(row);
+}
+
+/**
+ * Create and populate a table to show the result details.
+ * @param {[]} detectedObjects
+ * @param {Object} parent
+ */
+function detectedObjectsTable(detectedObjects, parent) {
+  if (detectedObjects.length > 0) {
+    const table = document.createElement('table');
+
+    addRow(table, 'th', ['Class', 'Confidence Score']);
+
+    for (let i = 0; i < detectedObjects.length; i++) {
+      const obj = detectedObjects[i];
+      const label = obj['class'];
+      const conf = obj['score'].toFixed(3);
+
+      addRow(table, 'td', [label, conf]);
+    }
+    parent.appendChild(table);
+  }
+}
+
+window.addEventListener('load', function() {
+  const article = document.querySelector('article');
+
+  /**
+   * Populate the article with formatted results.
+   * @param {Object} jsonResult
+   */
+  function populateArticle(jsonResult) {
+    // Remove previous results
+    article.innerHTML = '';
+
+    if (jsonResult.hasOwnProperty('data')) {
+
+      let classified = jsonResult.data.classes;
+      const myCount = document.createElement('h3');
+      myCount.textContent = "Watson sees...";
+      article.appendChild(myCount);
+      detectedObjectsTable(classified, article);
+    } else {
+      const myDiv = document.createElement('div');
+      myDiv.className = 'error';
+      myDiv.id = 'error-div';
+      const myTitle = document.createElement('h3');
+      myTitle.textContent = 'ERROR';
+      myDiv.appendChild(myTitle);
+      // Dump keys/values to show error info
+      for (const key in jsonResult) {
+        if (jsonResult.hasOwnProperty(key)) {
+          const myP = document.createElement('p');
+          myP.textContent = key + ':  ' + jsonResult[key];
+          myDiv.appendChild(myP);
+        }
+      }
+      article.appendChild(myDiv);
+    }
+  }
+
+  // When upload results are loaded (hidden), use them build the results.
+  const raw = top.frames['mytarget'];
+  const myTarget = document.getElementById('mytarget');
+  if (myTarget) { // optional for tests
+    myTarget.addEventListener('load', function() {
+      let rawContent = raw.document.body.innerText;
+      console.log(rawContent);
+      let rawJson = JSON.parse(rawContent);
+      console.log(rawJson);
+      populateArticle(rawJson);
+    });
+  }
+
+  // image preview
+  document.getElementById("foo").addEventListener("change", imagePreview);
+  function imagePreview(event) {
+    console.log(event);
+    let input = event.srcElement;
+    console.log(input.files);
+    console.log(input.files[0]);
+    if (input.files && input.files[0]) {
+      let fileReader = new FileReader();
+
+      fileReader.onload = function (e) {
+        const imagePreview = document.getElementById('imagePreview');
+        imagePreview.src = e.target.result;
+        imagePreview.style = "";
+      };
+
+      fileReader.readAsDataURL(input.files[0]);
+    }
+  }
+});
